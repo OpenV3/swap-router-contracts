ae7e377f011818f307d173e3c73a4112ce9ea927
Mo Shaikjee <shaikjeemohammed@gmail.com>
2024-12-05 13:18:15 +0200

    add ignition script

---
+++ ignition/modules/SwapRouterContracts.ts
@@ -0,0 +1,43 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+import { MISC_INFO } from "../../scripts/misc-addresses";
+import { getChainId } from "../../scripts/utils";
+
+export default buildModule("SwapRouterContracts", (m) => {
+  const chainId = getChainId();
+
+  // These addresses should be configured per network
+  const FACTORY_V3 = MISC_INFO[chainId]?.POOL_FACTORY!;
+  const WRAPPED_NATIVE = MISC_INFO[chainId]?.WRAPPED_NATIVE!;
+  const POSITION_MANAGER = MISC_INFO[chainId]?.NONFUNGIBLE_POSITION_MANAGER!;
+
+  // Using address(0) for factoryV2 as specified
+  const FACTORY_V2 = "0x0000000000000000000000000000000000000000";
+
+  // Deploy MixedRouteQuoterV1
+  const mixedRouteQuoter = m.contract(
+    "MixedRouteQuoterV1",
+    [
+      FACTORY_V3,    // _factory (V3)
+      FACTORY_V2,    // _factoryV2 (address(0))
+      WRAPPED_NATIVE // _WETH9
+    ]
+  );
+
+  // Deploy SwapRouter02
+  const swapRouter02 = m.contract(
+    "SwapRouter02",
+    [
+      FACTORY_V2,      // _factoryV2 (address(0))
+      FACTORY_V3,      // factoryV3
+      POSITION_MANAGER, // _positionManager
+      WRAPPED_NATIVE   // _WETH9
+    ]
+  );
+
+  // Return all deployed contracts
+  return {
+    mixedRouteQuoter,
+    swapRouter02
+  };
+});
